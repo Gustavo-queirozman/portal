e1bdb778e46a596d5af7aa07b33ba46d5775848e
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2023-09-25 07:38:13 -0300

    Criado Controller Usuário

---
+++ app/Http/Controllers/Usuario/CriarUsuarioController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Usuario;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CriarUsuarioController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}

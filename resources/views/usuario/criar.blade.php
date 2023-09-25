7fdc56b673a0a8db527e30e043423d432791706c
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2023-09-25 07:57:14 -0300

    Criado View Criar Usuário

---
+++ resources/views/usuario/criar.blade.php
@@ -0,0 +1,17 @@
+<form action="" method="post">
+    <select name="perfil">
+        <option value="Administrador">Administrador</option>
+        <option value="Prestador">Prestador</option>
+        <option value="Cooperado">Cooperado</option>
+        <option value="Cliente">Cliente</option>
+        <option value="Contratante">Contratante</option>
+        <option value="Financeiro/Cobrança">Financeiro/Cobrança</option>
+        <option value="Gestão de contratos">Gestão de contratos</option>
+    </select>
+    <input type="text" name="nome">
+    <input type="email" name="email">
+    <input type="text" name="login">
+    <input type="text" name="senha">
+    <input type="text" name="confirmar_senha">
+    <input type="submit" value="Entrar">
+</form>

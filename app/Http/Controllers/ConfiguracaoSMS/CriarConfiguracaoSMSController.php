80fd4044e42dc2258c92edee2e3e409ea81a38e9
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2023-09-27 11:54:19 -0300

    Criado Configuracao SMS Controller

---
+++ app/Http/Controllers/ConfiguracaoSMS/CriarConfiguracaoSMSController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\ConfiguracaoSMS;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CriarConfiguracaoSMSController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
